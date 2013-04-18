75e27e100318b618bcef35eb70edb4ff2da69820
Crina Bucur <bucur.crina@gmail.com>
2013-04-18 13:36:40 +0300

    IBAction createRace empty method yet

--- JustRace/NewRaceViewController.h
+++ JustRace/NewRaceViewController.h
@@ -11,13 +11,29 @@
 @interface NewRaceViewController : UIViewController <UITextFieldDelegate>{
 
     NSDate *raceDate;
+    NSDate *raceTime;
     UIActionSheet *dateSheet;
-    
+    IBOutlet UITextField *dateTextField;
+    IBOutlet UITextField *raceTimeTextField;
+    IBOutlet UITextField *startPointTextField;
+    IBOutlet UITextField *endPointTextField;
+    IBOutlet UITextField *lengthTextField;
+    //IBOutlet UITextView *mottoTextView;
+    IBOutlet UITextField *raceNameTextField;
 }
 @property (strong, nonatomic) IBOutlet UITextField *dateTextField;
+@property (strong, nonatomic) IBOutlet UITextField *raceTimeTextField;
+@property (strong, nonatomic) IBOutlet UITextField *startPointTextField;
+@property (strong, nonatomic) IBOutlet UITextField *endPointTextField;
+@property (strong, nonatomic) IBOutlet UITextField *lengthTextField;
+@property (strong, nonatomic) IBOutlet UITextView *motto;
+@property (strong, nonatomic) IBOutlet UITextField *raceNameTextField;
 @property (strong, nonatomic) NSDate *raceDate;
+@property (strong, nonatomic) NSDate *raceTime;
 
 -(void) setRaceDate;
+-(void) setRaceTime;
 -(void) dismissDateSet;
 -(void) cancelDateSet;
+-(IBAction)createRace:(id)sender;
 @end
--- JustRace/NewRaceViewController.m
+++ JustRace/NewRaceViewController.m
@@ -14,7 +14,14 @@
 
 @implementation NewRaceViewController
 @synthesize raceDate;
+@synthesize raceTime;
+@synthesize raceTimeTextField;
 @synthesize dateTextField;
+@synthesize startPointTextField;
+@synthesize endPointTextField;
+@synthesize lengthTextField;
+@synthesize motto;
+@synthesize raceNameTextField;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -69,6 +76,38 @@
     
 }
 
+-(void) setRaceTime{
+    dateSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:nil cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil, nil];
+    
+    [dateSheet setActionSheetStyle:UIActionSheetStyleBlackTranslucent];
+    
+    CGRect pickerFrame= CGRectMake(0,44,0,0);
+    UIDatePicker *raceTimePicker = [[UIDatePicker alloc] initWithFrame:pickerFrame];
+    [raceTimePicker setDatePickerMode:UIDatePickerModeTime];
+    
+    [dateSheet addSubview:raceTimePicker];
+    
+    UIToolbar *controlToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, dateSheet.bounds.size.width, 44)];
+    
+    [controlToolBar setBarStyle:UIBarStyleBlack];
+    [controlToolBar sizeToFit];
+    
+    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    UIBarButtonItem *setButton = [[UIBarButtonItem alloc] initWithTitle:@"Set" style:UIBarButtonItemStyleDone target:self action:@selector(dismissDateSet)];
+    
+    UIBarButtonItem *cancelButton =[[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelDateSet)];
+    
+    [controlToolBar setItems:[NSArray arrayWithObjects:spacer, cancelButton, setButton, nil] animated:NO];
+    
+    [dateSheet addSubview:controlToolBar];
+    
+    [dateSheet showInView:self.view];
+    
+    [dateSheet setBounds:CGRectMake(0, 0,320, 485)];
+
+}
+
 -(void)cancelDateSet{
     
     [dateSheet dismissWithClickedButtonIndex:0 animated:YES];
@@ -77,26 +116,40 @@
 -(void)dismissDateSet{
     
     NSArray *listOfViews = [dateSheet subviews];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     
     for(UIView *subView in listOfViews){
         if ([subView isKindOfClass:[UIDatePicker class]]){
-            self.raceDate = [(UIDatePicker *) subView date];
+            if ([(UIDatePicker *) subView datePickerMode] == UIDatePickerModeDate){
+                self.raceDate = [(UIDatePicker *) subView date];
+                [dateFormatter setDateFormat:@"MM/dd/yyyy"];
+                [dateTextField setText:[dateFormatter stringFromDate:self.raceDate]];
+                
+            }
+            if ([(UIDatePicker *) subView datePickerMode] == UIDatePickerModeTime){
+                self.raceTime = [(UIDatePicker *) subView date ];
+                [dateFormatter setDateFormat:@"hh:mm"];
+                [raceTimeTextField setText:[dateFormatter stringFromDate:self.raceTime]];
+            }
         }
     }
-    
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    
-    [dateFormatter setDateFormat:@"MM/dd/yyyy"];
-    
-    [dateTextField setText:[dateFormatter stringFromDate:self.raceDate]];
     
     [dateSheet dismissWithClickedButtonIndex:0 animated:YES];
 
 }
 
 -(BOOL) textFieldShouldBeginEditing:(UITextField *)textField{
+    if ([textField isEqual:dateTextField]){
     [self setRaceDate];
+    }
+    if ([textField isEqual:raceTimeTextField]){
+        [self setRaceTime];
+    }
     return NO;
+    
+}
+
+-(IBAction)createRace:(id)sender{
     
 }
 @end
