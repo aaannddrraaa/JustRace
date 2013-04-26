821e36aba94a04846d4315a201f4c34b90379d29
Laborator iOS <laboratorios@Cronian-Mac-3.local>
2013-04-26 18:21:52 +0300

    Attended races [solved]

--- JustRace/AllAttendedViewController.h
+++ JustRace/AllAttendedViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AllAttendedViewController : UITableViewController
+@interface AllAttendedViewController : UITableViewController<UITableViewDataSource, UITableViewDelegate>{
+    NSArray *attendedRaces;
+}
+
 
 @end
