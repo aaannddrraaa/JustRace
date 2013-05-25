acc9440e923ff33556c8030efad625b2405f722c
cdalxndr <cdalxndr@yahoo.com>
2013-05-25 13:09:16 +0300

    added view for results

---
+++ JustRace/ResultsCell.m
@@ -0,0 +1,29 @@
+//
+//  ResultsCell.m
+//  JustRace
+//
+//  Created by Laborator iOS on 5/25/13.
+//  Copyright (c) 2013 Andra Mititelu. All rights reserved.
+//
+
+#import "ResultsCell.h"
+
+@implementation ResultsCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ JustRace/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  JustRace
+//
+//  Created by Laborator iOS on 5/25/13.
+//  Copyright (c) 2013 Andra Mititelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UITableViewController
+
+@end
